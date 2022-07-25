6ec055a2fa018856bde745801e3b7b542e190a16
Dlord MDia <109800409+dlordmdia@users.noreply.github.com>
2022-07-25 23:31:24 +0200

    MY FIRST CODE EVER

---
+++ evaluación.py
@@ -0,0 +1,7 @@
+nota = float(input("A que nota te han evaluado en tú último examen? "))
+
+if nota >= 5: 
+    print("Bieeen!!! Has aprobado! Estoy orgullos@! ")
+
+else:
+    print("Nooo... Has suspendido... :( Sorry... ")
